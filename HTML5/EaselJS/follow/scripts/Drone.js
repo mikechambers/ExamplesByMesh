3bf4d393ca6f8867e4f223f94727bafb092574ed
Mike Chambers <mikechambers@gmail.com>
2011-01-20 23:04:43 -0800

    Initial checkin of EaselJS example that shows how to make additive graphics and how to restore canvas data after resize.

---
+++ HTML5/EaselJS/follow/scripts/Drone.js
@@ -0,0 +1,146 @@
+/*
+	The MIT License
+
+	Copyright (c) 2011 Mike Chambers
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+//constructor for Drone
+function Drone()
+{
+	//set the radius to the default radius
+	this.radius = Drone.DEFAULT_RADIUS;
+	
+	//set fill color
+	this.fillColor = Graphics.getRGB(255,255,255);
+	
+	//set stroke color
+	this.strokeColor = Graphics.getRGB(0, 0, 0, .75);
+	
+	//draw ourselves
+	this.$draw();
+}
+
+//extends EaselJS Shape
+Drone.prototype = new Shape();
+
+//How fast the drone moves
+Drone.prototype.speed = 5;
+
+//temp object we use to pass around point data
+//so we can reuse and dont have to keep reinstantiating
+Drone.prototype.p2 = new Point(0,0);
+
+//direction the radius tween is moving in
+Drone.prototype.reverse = false;
+
+//current radius
+Drone.prototype.radius = 5;
+
+//default radius
+Drone.DEFAULT_RADIUS = 5;
+
+//max radius
+Drone.MAX_RADIUS = 50;
+
+//determines whether radius increases (1) or
+//decreases(-1)
+Drone.prototype.mod = 1;
+
+//graphics fill color
+Drone.prototype.fillColor = null;
+
+//graphics stroke color
+Drone.prototype.strokeColor = null;
+
+//draw function
+Drone.prototype.$draw = function()
+{
+	//check to see if radius is over 50, or smaller
+	//than the default radius, if it is, change the 
+	//direction of the size tween.
+	if(this.radius > Drone.MAX_RADIUS || this.radius < Drone.DEFAULT_RADIUS)
+	{
+		this.reverse = !this.reverse;
+		
+		//determine whether we should increase or
+		//decrease radius size		
+		this.mod = (this.reverse)?-1:1;
+	}	
+
+	//update the radius
+	this.radius += this.mod;
+	
+	//create a new Graphics to draw our shape
+	var g = new Graphics();
+	
+	//set the stroke to 1 px
+	g.setStrokeStyle(1);
+	
+	g.beginFill(this.fillColor);
+	g.beginStroke(this.strokeColor);
+	
+	//some code to tween between colors (from Grant Skinner)
+	//60 narrows the range, 180 determine where the color is
+	//g.beginFill(Graphics.getHSL(Math.cos((this.count++)*0.1)*60 + 180, 100, 50));
+	
+	//draw the circle
+	g.drawCircle(0,0,this.radius);
+	
+	//move drawing point to center
+	g.moveTo(0,0);
+	
+	//draw another smaller circle
+	g.drawCircle(0,0,2);
+	g.endFill();		
+	
+	//set the graphics property to our Graphics. This 
+	//will be used the the Shape baseclass to render
+	//our graphics
+	this.graphics = g;
+}
+
+//called automatically since we subclass Shape.
+//called at each time interval
+Drone.prototype.tick = function()
+{
+	this.update();
+}
+
+Drone.prototype.update = function()
+{				
+	//copy our coordinates into the Point instance
+	this.p2.x = this.x;
+	this.p2.y = this.y;
+
+	//get angle from enemy to target / ship
+	var radians = MathUtil.getAngleBetweenPoints(Mouse, this.p2);
+
+	//determine velocity on x and y axis
+	var vx = Math.cos(radians) * this.speed;
+	var vy = Math.sin(radians) * this.speed;
+
+	//update position
+	this.x += vx;
+	this.y += vy;
+
+	//redraw
+	this.$draw();
+}
---
+++ HTML5/EaselJS/follow/scripts/Main.js
@@ -0,0 +1,190 @@
+/*
+	The MIT License
+
+	Copyright (c) 2011 Mike Chambers
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+$(document).ready(init);
+
+var stage;
+var canvasWrapper;
+var canvasOffset;
+
+var drone;
+
+//Simple object to store Mouse coordinates. Used to make
+//it easy for other classes to access the data
+var Mouse = {x:0,y:0};
+
+
+//called once the page has loaded
+function init()
+{
+	//get a reference to the canvas element
+	canvasWrapper = $("#mainCanvas");
+	
+	//check for canvas support
+	if(!(!!document.createElement('canvas').getContext))
+	{
+		////document.createElement("article");
+		canvasWrapper.html("<div>" +
+			"It appears you are using a browser that does not support "+
+			"the HTML5 Canvas Element</div>");
+			
+			return;
+	}
+		
+	//update the dimensions of the canvas
+	updateCanvasDimensions();
+	
+	//initialize the Stage instance with the Canvas. Note, 
+	//canvasWrapper is a JQuery object, but stage expects
+	//the actual Canvas element.
+	stage = new Stage(canvasWrapper.get(0));
+	
+	//set autoClear to false, so the stage is not
+	//cleared between renders (when stage.tick()) is called
+	stage.autoClear = false;
+	
+	//listen for when the window resizes
+	$(window).resize(onWindowResize);
+	
+	//listen for when the window looses focus
+	$(window).blur(onWindowBlur);
+	
+	//listen for a click event
+	canvasWrapper.click(onMouseClick);
+	
+	//listen for when the mouse moves
+	canvasWrapper.mousemove(onMouseMove);	
+	
+	//create our Drone instance, that will
+	//follow our mouse
+	drone = new Drone();
+	
+	//position it just off the screen on the left
+	drone.y = 20;
+	drone.x = -Drone.DEFAULT_RADIUS * 2;
+	
+	//add the drone to the stage
+	stage.addChild(drone);
+
+	//set the tick interval to 24 frames per second.
+	Tick.setInterval(1000/24);
+	
+	//listen for the tick event (time event)
+	//note, the secound argument allows this listener
+	//to be paused
+	Tick.addListener(window, true);
+	
+	//pause the Tick instance, so it doesnt broadcast
+	//the tick event. (We will unpause when the user clicks
+	//the canvas)
+	Tick.setPaused(true);
+}
+
+//called when the user clicks the mouse on the canvas
+function onMouseClick(e)
+{
+	//toggle the Tick / Time paused state.
+	Tick.setPaused(!Tick.getPaused());
+}
+
+//called when the window looses focus
+function onWindowBlur(e)
+{
+	//pause the Tick / time manager
+	//this is so it doesnt keep drawing / using CPU
+	//when the user switches away
+	Tick.setPaused(true);
+}
+
+//called at each time inteval. This is essentially the listener
+//for Tick.addListener
+function tick()
+{
+	//check if we are paused
+	if(!Tick.getPaused())
+	{
+		//if not, render the stage
+		stage.tick();
+	}
+}
+
+//called when the mouse is moved over the canvas
+function onMouseMove(e)
+{
+	//update the Mouse position coordinates
+	updateMouseCoordinates(e);
+}
+
+//called when the browser window is resized
+function onWindowResize(e)
+{
+	//right now, the stage instance, doesnt expose the canvas
+	//context, so we have to get a reference to it ourselves
+	var context = canvasWrapper.get(0).getContext("2d");
+	
+	//copy the image data from the current canvas
+	var data = context.getImageData(0, 0, 
+			canvasWrapper.attr("width"), 
+			canvasWrapper.attr("height"));
+			
+			
+	//update the canvas dimensions since the window
+	//has resized. Note that changing canvas dimensions, 
+	//will cause it to be cleared
+	updateCanvasDimensions();
+	
+	//copy the data back onto the resized canvas. It is possible
+	//that if the previous canvas was larger than the newly sized one
+	//that we will loose some pixels (as they will be cut off)
+	context.putImageData(data, 0,0);
+	
+	//note, we only have to do this because we have stage.autoClear set
+	//to false, otherwise, the stage instance could redraw the entire canvas
+	
+	//only rerender the stage if the Tick manager is not paused
+	if(!Tick.getPaused())
+	{
+		stage.tick();
+	}
+}
+
+//function that updates the size of the canvas based on the window size
+function updateCanvasDimensions()
+{
+	//note that changing the canvas dimensions clears the canvas.
+	canvasWrapper.attr("height", $(window).height(true));
+	canvasWrapper.attr("width", $(window).width(true));
+	
+	//save the canvas offset
+	canvasOffset = canvasWrapper.offset();
+}
+
+//update the mouse coordinates
+function updateMouseCoordinates(e)
+{
+	//we store these in a global object so they can be easily acccessed
+	//from anywhere (other classes)
+	Mouse.x = e.pageX - canvasOffset.left;
+	Mouse.y = e.pageY - canvasOffset.top;
+}
